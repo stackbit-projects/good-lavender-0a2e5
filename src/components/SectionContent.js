59fe260558a26d325a4d1c311caf8e3bf7de294e
Stackbit Code Editor <projects@stackbit.com>
2021-11-03 02:56:10 +0000

    SectionContent.js: changed

--- src/components/SectionContent.js
+++ src/components/SectionContent.js
@@ -22,7 +22,8 @@ export default class SectionContent extends React.Component {
                         <img src={withPrefix(image)} alt={imageAlt} />
                     </div>
                 )}
-                {content && <div className="block-content inner-sm text-center">{markdownify(content)}</div>}
+                {content && <div className="block-content inner-sm text
+                ">{markdownify(content)}</div>}
                 {!_.isEmpty(actions) && (
                     <div className="block-buttons inner-sm">
                         <CtaButtons actions={actions} />
